6c21a6801812b0165a9c46e08c89cc7598eff9f0
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 13:19:26 +0100

    correction

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,10 +4,11 @@ import random
 number = random.randint(-10000, 10000)
 
 # Extract the last digit of the number
-last_digit = number % 10
+last_digit = abs(number) % 10
+last_digit_with_sign = last_digit if number >= 0 else -last_digit
 
 # Print the message with the number and its last digit using f-string
-print(f"Last digit of {number} is {last_digit} and is", end=" ")
+print(f"Last digit of {number} is {last_digit_with_sign} and is", end=" ")
 
 # Check whether the last digit is greater than 5, 0, or less than 6 but not 0 and print the corresponding message
 if last_digit > 5:
