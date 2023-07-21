d23c40a83b7b205acb7dbbd7d99e9adf9c3ecd42
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 12:35:45 +0100

    changes

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,6 +1,6 @@
 # a program that prints all numbers from 0 to 98 in decimal and in hexadecimal
 #!/usr/bin/python3
-for num in range(100):
+for num in range(99):
     print("{} = {:#x}".format(num, num))
 
 
