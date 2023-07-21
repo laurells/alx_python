fbc49ec520fa18d526d693907bd38e763f269688
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 14:29:30 +0100

    improvement

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,16 @@
 def fibonacci_sequence(n):
     if n <= 0:
         return []
-    
-    fibonacci_numbers = [0, 1]
-    
+
+    fibonacci_numbers = [0]
+
+    if n == 1:
+        return fibonacci_numbers
+
+    fibonacci_numbers.append(1)
+
     while len(fibonacci_numbers) < n:
         next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
         fibonacci_numbers.append(next_number)
-        
+
     return fibonacci_numbers
