e507dd5d05076faafe438c896cbb7f21282a6a4e
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 12:37:41 +0100

    changes

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,4 +1,5 @@
 # a program that prints numbers from 0 to 99
 #!/usr/bin/python3
 for num in range(100):
-    print(f"{num:02d}", end=(", " if num < 99 else "\n"))
+    print("{:02d}".format(num), end=(", " if num < 99 else "\n"))
+
