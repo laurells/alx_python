df50ea81770ee4e8d8a7f642bda6b2bd4eb64540
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 12:41:47 +0100

    changes

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        print("{:d}{:1d}".format(num1, num2), end=" " if (num1, num2) != (9, 8) else "/n")
+        print("{:d}{:1d}".format(num1, num2), end=", " if (num1, num2) != (9, 8) else "\n")
