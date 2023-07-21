f23831e18fd623cacfc8b7da49036d2f9fb61d76
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 12:16:07 +0100

    update 1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print(word_first_3)
-print(word_last_2)
-print(middle_word)
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
