973c88ffcb06d35470701f79a946a54ee9b3aeb9
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 13:15:28 +0100

    changes

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        print("{:d}{:1d}".format(num1, num2), end=", " if (num1, num2) != (9, 8) else "\n")
+        print("{:02d}".format(num1 * 10 + num2), end=(", " if num1 < 8 else "\n"))
