98a10f5343ec30d66cd164196a4744cdf1834c00
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 12:23:33 +0100

    changes

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -4,13 +4,10 @@ import random
 # Assign a random signed number to the variable 'number'
 number = random.randint(-100, 100)
 
-# Print the number
-print(number, end=" ")
-
 # Check whether the number is positive, negative, or zero and print the corresponding message
 if number > 0:
-    print("is positive")
+    print(f"{number} is positive")
 elif number == 0:
-    print("is zero")
-elif number < 0:
-    print("is negative")
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
