660aba3efadce19a97e31d10937aa7d09781ae58
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-21 12:44:17 +0100

    changes

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,4 @@
-pairs = []
+#!/usr/bin/python3
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        pairs.append("{:d}{:1d}".format(num1, num2))
-
-print(", ".join(pairs))
+        print("{:d}{:1d}".format(num1, num2), end=", " if (num1, num2) != (9, 8) else "\n")
