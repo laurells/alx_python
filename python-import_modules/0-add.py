258617e939526c5cde51dd36e0d6a5007046e4af
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-25 17:18:10 +0100

    error handled

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+if __name == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
