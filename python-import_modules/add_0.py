e3b799a94db78bfd274946cd66c5ee9fd3afa359
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-25 17:21:32 +0100

    add_0 updated

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,2 +1,7 @@
 def add(a, b):
     return a + b
+
+if __name__ == "__main__":
+    # The code inside this block will only execute when the script is run directly
+    # and not when it's imported as a module
+    pass
