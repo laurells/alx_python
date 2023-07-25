8ba2cd722cf085356be4d906371dbdb84d991813
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-25 17:50:03 +0100

    changes

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -4,3 +4,9 @@ class NameException(Exception):
 def raise_exception_msg(message=""):
     raise NameException(message)
 
+if __name__ == "__main__":
+    try:
+        message = "C is fun"
+        raise_exception_msg(message)
+    except NameException as e:
+        print(e)
